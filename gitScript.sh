58050caeec0cebd180fd075d9341d1e3c373f137
Light2027 <43041394+Light2027@users.noreply.github.com>
2021-11-13 19:30:04 +0100

    Delete gitScript.sh

--- gitScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "# Node-Test" >> README.md
-git init
-git add .
-git commit -m "Initial commit"
-git branch -M main
-git remote add origin https://github.com/Light2027/Node-Test.git
-git push -u origin main
